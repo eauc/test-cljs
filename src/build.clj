aa85bf60bf0efd61a2ae562493fc94ba6ed22808
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2018-05-12 21:25:11 +0200

    Setup index page

--- src/build.clj
+++ src/build.clj
@@ -1,10 +1,27 @@
 (ns build
   (:require [cljs.build.api :as b]))
 
+(def source-path "src")
+
+(def dev-options
+  {:main 'hello-world.core
+   :output-to "resources/public/js/main.js"
+   :output-dir "resources/public/js/out"
+   :asset-path "js/out"
+   :optimizations :none
+   :verbose true})
+
+(defmulti build
+  (fn [name] name))
+
+(defmethod build :default
+  [name & rest]
+  (println "Unknown \"" name "\" profile"))
+
+(defmethod build "dev"
+  [& args]
+  (println "Building \"dev\" profile...")
+  (b/build source-path dev-options))
+
 (defn -main [& args]
-  (println "Building project..." args)
-  (b/build "src"
-           {:main 'hello-world.core
-            :output-to "out/main.js"
-            :output-dir "out"
-            :optimizations :none}))
+  (apply build (or args '("dev"))))
